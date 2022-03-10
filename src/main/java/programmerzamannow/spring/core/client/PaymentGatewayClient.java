2170f628410b3018be7195010f6ccaff145a90b3
ongky-febriandy <febriandy.ongky@gmail.com>
2022-03-10 13:34:10 +0700

    Factory Bean

---
+++ src/main/java/programmerzamannow/spring/core/client/PaymentGatewayClient.java
@@ -0,0 +1,14 @@
+package programmerzamannow.spring.core.client;
+
+
+import lombok.Data;
+
+@Data
+public class PaymentGatewayClient {
+
+    private String endpoint;
+
+    private String privateKey;
+
+    private String publicKey;
+}
