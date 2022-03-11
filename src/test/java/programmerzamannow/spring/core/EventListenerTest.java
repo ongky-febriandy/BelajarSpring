b15b33ac2c4644de33e96a91ad7fd89f746bfdd5
ongky-febriandy <febriandy.ongky@gmail.com>
2022-03-11 11:06:09 +0700

    Event Listener

---
+++ src/test/java/programmerzamannow/spring/core/EventListenerTest.java
@@ -0,0 +1,39 @@
+package programmerzamannow.spring.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import programmerzamannow.spring.core.listener.LoginSuccessListener;
+import programmerzamannow.spring.core.service.UserService;
+
+public class EventListenerTest {
+
+    @Configuration
+    @Import({
+            UserService.class,
+            LoginSuccessListener.class
+    })
+    public static class TestConfiguration{
+
+    }
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(TestConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testEvent() {
+
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.login("ongky", "ongky");
+        userService.login("ongky", "salah");
+        userService.login("febriandy", "salah");
+    }
+}
