f1a8ed85d7b074f19732668a60cfa435e49083fc
ongky-febriandy <febriandy.ongky@gmail.com>
2022-03-11 14:00:44 +0700

    Spring Boot Test application

---
+++ src/main/java/programmerzamannow/spring/core/application/FooApplication.java
@@ -0,0 +1,24 @@
+package programmerzamannow.spring.core.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import programmerzamannow.spring.core.data.Foo;
+
+@SpringBootApplication
+public class FooApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo);
+    }
+
+    @Bean
+    public Foo foo(){
+        return new Foo();
+    }
+}
